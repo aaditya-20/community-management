dfd103eaf4ad7274f44cca957104c03cc5e4ebe2
aaditya-20 <91070772+aaditya-20@users.noreply.github.com>
2023-04-04 16:26:21 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
-import { FaDiscord, FaTwitter } from 'react-icons/fa';
-import IconButton from '../components/atoms/IconButton';
-import { ReactElement } from 'react';
 
-const HomePage = (): ReactElement => {
-  const handleDiscordClick = () => {
-    // handle Discord button click
-  };
-
-  const handleTwitterClick = () => {
-    // handle Twitter button click
-  };
-
+const HomePage = () => {
+ 
   return (
     <div>
       <h1>Welcome to my app!</h1>
-      <IconButton icon={FaDiscord} label="Join " className="bg-indigo-600" onClick={handleDiscordClick} />
-      {/* <IconButton icon={FaTwitter} label="Follow us on Twitter" className="bg-blue-500" onClick={handleTwitterClick} /> */}
+      
     </div>
   );
 };
