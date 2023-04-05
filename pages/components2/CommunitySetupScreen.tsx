2f22506df13db0833ff79c9c0990e15209570451
Aaditya <codingaadi@gmail.com>
2023-04-05 10:10:11 +0530

    added some code for community set up

---
+++ pages/components2/CommunitySetupScreen.tsx
@@ -0,0 +1,8 @@
+import Card from "../components/atoms/Card"
+export default function CommunitySetupScreen() {
+  return (
+    <>
+      <Card/>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
 import { FaDiscord, FaTwitter } from 'react-icons/fa';
 import IconButton from '../components/atoms/IconButton';
 import { ReactElement } from 'react';
-
+import Card from "../components/atoms/Card"
+import Link from 'next/link'
 const HomePage = (): ReactElement => {
-  const handleDiscordClick = () => {
-    // handle Discord button click
-  };
-
-  const handleTwitterClick = () => {
-    // handle Twitter button click
-  };
 
   return (
-    <div>
-      <h1>Welcome to my app!</h1>
-      <IconButton icon={FaDiscord} label="Join " className="bg-indigo-600" onClick={handleDiscordClick} />
-      {/* <IconButton icon={FaTwitter} label="Follow us on Twitter" className="bg-blue-500" onClick={handleTwitterClick} /> */}
-    </div>
+    <>
+      <Card title="" text="" />
+       <div className='absolute w-[662px] h-[54px] left-[350px] top-[140px] border-b-[1px]'>
+       <Link href="/" className='relative top-[15px] left-[30px] text-[#AEABD8]'>SIGN UP</Link>
+        
+        </div>
+        <div className='absolute  left-[350px] top-[220px]'>
+          <p className='relative w-[236px] h-[32px] top-[0px] left-[30px] text-[#FFFFFF] font-sans font-normal font-bold:text-600 text-2xl leading-8'>What do we call you?</p>
+          <p className='relative w-[200px] h-[22px] top-[10px] left-[30px] font-sans font-normal text-base leading-6 text-gray-500'>Tell us your name?</p>
+        </div>
+      
+    </>
   );
 };
 
