17f4489c42412a6b6f7b8d1941a4d0f277e23e21
Aps2612 <apsingh1722@gmail.com>
2023-04-13 22:14:55 +0530

    feat: :sparkles: Added Text Area Atom

--- components/atoms/TextArea.tsx
+++ components/atoms/TextArea.tsx
@@ -7,7 +7,7 @@ type TextAreaProps = {
   textAreaCSS?: string;
 };
 
-const TextArea = ({ label, placeholder = '', className = '', textAreaCSS = '' }: TextAreaProps): ReactElement => {
+const TextArea = ({ label, placeholder = '', className = '',LabelCSS=' ', textAreaCSS = '' }: TextAreaProps): ReactElement => {
   // state to hold the current input value
   const [value, setValue] = useState('');
 
@@ -21,14 +21,14 @@ const TextArea = ({ label, placeholder = '', className = '', textAreaCSS = '' }:
   return (
     <div className={`flex flex-col ${className}`}>
       {/* label for the textarea */}
-      <label className="text-sm font-medium text-gray-700">{label}</label>
+      <label className={`text-sm font-medium text-gray-700 ${LabelCSS}`}>{label}</label>
 
       {/* the textarea element itself */}
       <textarea
         value={value} // set the value of the textarea to the current state
         onChange={handleChange} // call handleChange on input change events
         placeholder={placeholder}
-        className={`px-3 py-2 mt-1 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${textAreaCSS}`}
+        className={`px-3 py-12 mt-1 overflow-hidden${textAreaCSS}`}
       />
     </div>
   );
