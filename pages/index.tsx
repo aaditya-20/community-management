635657d564b7b737e1e0dd4bcb62678917fd05d0
Aaditya <codingaadi@gmail.com>
2023-04-19 11:34:36 +0530

    fix: :bug: build fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import CommunitySetupScreen from '@/components/organism/CommunitySetupScreen'
 import AnalyticsCardMainDashboard from '@/components/molecules/AnalyticsCardMainDashboard'
-import StackedBarChart from '@/components/molecules/Temp'
+
 import MemberGrowthCardAnalyticsScreen from '@/components/molecules/MemberGrowthCardAnalyticsScreen'
 import CommunitySetUpIntegration from '@/components/organism/CommunitySetUpIntegration'
-import Temp from '@/components/molecules/Temp'
+
 export default function index() {
   return (
     <div>
