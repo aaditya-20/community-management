3e41b0aaf70523bc29104faaaf46207939ac0fc0
Aps2612 <apsingh1722@gmail.com>
2023-04-20 16:00:25 +0530

    feat: :sparkles: Added Header,ShowNotifications and TextArea

--- components/atoms/TextArea.tsx
+++ components/atoms/TextArea.tsx
@@ -4,10 +4,17 @@ type TextAreaProps = {
   label: string;
   placeholder?: string;
   className?: string;
-  textAreaCSS?: string;
+  classNameLabel?: string;
+  classNameInput?: string;
 };
 
-const TextArea = ({ label, placeholder = '', className = '',LabelCSS=' ', textAreaCSS = '' }: TextAreaProps): ReactElement => {
+const TextArea = ({
+  label,
+  placeholder = '',
+  className = '',
+  classNameLabel = '',
+  classNameInput = '',
+}: TextAreaProps): ReactElement => {
   // state to hold the current input value
   const [value, setValue] = useState('');
 
@@ -21,14 +28,15 @@ const TextArea = ({ label, placeholder = '', className = '',LabelCSS=' ', textAr
   return (
     <div className={`flex flex-col ${className}`}>
       {/* label for the textarea */}
-      <label className={`text-sm font-medium text-gray-700 ${LabelCSS}`}>{label}</label>
+      <label className={`text-sm font-medium text-gray-700 ${classNameLabel}`}>{label}</label>
 
       {/* the textarea element itself */}
       <textarea
         value={value} // set the value of the textarea to the current state
         onChange={handleChange} // call handleChange on input change events
         placeholder={placeholder}
-        className={`px-3 py-12 mt-1 overflow-hidden${textAreaCSS}`}
+        className={`px-3 py-2 mt-1 overflow-hidden text-white font-small ${classNameInput}`}
+        style={{ height: '150px', resize: 'none' }} // set the height of the textarea
       />
     </div>
   );
