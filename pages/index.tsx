6864084e78ddb4a4b7439acc3619c98e2456e710
Aaditya <codingaadi@gmail.com>
2023-04-20 16:49:01 +0530

    feat: :art: added some code for member growth card

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,13 @@ import { AiOutlineUp } from "react-icons/ai";
 import { FaDiscord } from 'react-icons/fa';
 import TopContributorCardLeaderboardScreen from '@/components/molecules/TopContributorCardLeaderboardScreen'
 import NewMemberCardAnalyticScreen from '@/components/molecules/NewMemberCardAnalyticScreen'
+import Member from '@/components/molecules/MemberListCardMissionPage'
 
 export default function index() {
   return (
     <div>
         {/* <MemberXpDetailsForTopContributor icon={FaDiscord} change={AiOutlineUp} colorOfChange="inline text-[red]" position={1}/> */}
-        <TopContributorCardLeaderboardScreen/>
+        <Member/>
     </div>
   )
 }
