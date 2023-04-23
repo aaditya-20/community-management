1177a5245a07b750a96729b34a1cd01bb3e9316c
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-04-24 02:46:36 +0530

    feat: :sparkles: Build Fix

--- components/organism/DiscordIntegrationPopup.tsx
+++ components/organism/DiscordIntegrationPopup.tsx
@@ -4,6 +4,9 @@ import IconButton from "../atoms/IconButton";
 import ProfileIcon from "../atoms/ProfileAvatar";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 export default function DiscordIntegrationPopup() {
+  function handleProfileClick(){
+    // Task:will divide icon in two types-Profile(avatar)+Uplaod Icons
+  }
   return (
     <>
       <Card
@@ -25,6 +28,7 @@ export default function DiscordIntegrationPopup() {
           imageUrl="Icons/RedFire.svg"
           size={87}
           classNameCircle="border-dashed border-[0.7px] border-white flex justify-center"
+          onProfileIconClick={handleProfileClick}
         />
       </div>
       <div className="absolute h-[6px] w-[73px] left-[756px] top-[410px] bg-[#3D3E45]"></div>
@@ -33,6 +37,7 @@ export default function DiscordIntegrationPopup() {
           imageUrl="Icons/Discord.svg"
           size={87}
           classNameCircle="border-dashed border-[0.7px] border-white flex justify-center"
+          onProfileIconClick={handleProfileClick}
         />
       </div>
       <div className="flex-wrap absolute flex justify-center left-[560px] top-[530px] w-[500px] h-[32px] text-[#A6A6A6] font-[General Sans] ">
@@ -44,6 +49,7 @@ export default function DiscordIntegrationPopup() {
           icon={FaDiscord}
           label="Discord"
           className="absolute bg-[#8570E4] top-[20px] left-[30px] w-[240px] h-[50px] rounded-md"
+          classNameIcon=""
         />
       </div>
       <div className="absolute top-[720px] left-[775px]">
