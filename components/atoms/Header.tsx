0452bf1114af63e84384db543543598b5d751d6b
Aaditya <codingaadi@gmail.com>
2023-04-24 18:15:16 +0530

    fix: :zap: Some modification in header
    some modification was done in header so that it will adjust automatically for all screen size

--- components/atoms/Header.tsx
+++ components/atoms/Header.tsx
@@ -4,8 +4,8 @@ let data = 0;
 
 const Header = () => {
   return (
-    <div className="relative box-border box-sizing border-box w-[1518px] h-[70px] left-[0px] top-0 bg-gradient-to-b from-gray-900 to-gray-800  border-b-1 border-[#171C23]">
-      <div className="absolute w-[124px] h-[39px] left-[1150px] top-[20px] bg-[#313131] rounded-[25px] flex items-center justify-center">
+    <div className="relative box-border box-sizing border-box w-[auto] h-[70px] left-[0px] top-0  bg-[#171C23]">
+      <div className="absolute w-[124px] h-[39px] right-[249px] top-[20px] bg-[#313131] rounded-[25px] flex items-center justify-center">
         <button type="button" className="text-white font-small">
           Link wallet
         </button>
@@ -13,7 +13,7 @@ const Header = () => {
 
      <ShowNotifications/>
 
-      <div className="absolute w-[152px] h-[39px] left-[1350px] top-[20px] bg-[#313131] rounded-[25px] flex items-center justify-center">
+      <div className="absolute w-[152px] h-[39px] right-[16px] top-[20px] bg-[#313131] rounded-[25px] flex items-center justify-center">
   <button type="button" className="text-gray-400 font-small flex items-center space-x-2">
     <img 
     src={"/Avatar.png"} 
--- components/atoms/ShowNotification.tsx
+++ components/atoms/ShowNotification.tsx
@@ -22,14 +22,14 @@ function ShowNotifications() {
         <img
           src="/icons/vector.svg"
           alt="My Icon"
-          className="h-5 w-5 ml-2 div absolute w-[30px] h-[25px] left-[1290px] top-[26px] bg-[#313131] rounded-[25px] flex items-center justify-center"
+          className=" ml-2 div absolute w-[30px] h-[25px] right-[195px] top-[26px] bg-[#313131] rounded-[25px] flex items-center justify-center"
         />
         {unread > 0 && (
-          <div className="ml-2 div absolute w-2 h-2 rounded-full bg-red-500 left-[1310px] top-[23px]"></div>
+          <div className="ml-2 div absolute w-2 h-2 rounded-full bg-red-500 right-[195px] top-[23px]"></div>
         )}
       </button>
       {unread>0 && showMessages && (
-        <div className="absolute z-10 w-64 bg-gray-400 shadow-lg rounded-lg py-2 mt-1 left-[1230px] top-[75px]">
+        <div className="absolute z-10 w-64 bg-gray-400 shadow-lg rounded-lg py-2 mt-1 right-[195px] top-[75px]">
           <ul>
             <li>Unread message 1</li>
             <li>Unread message 2</li>
