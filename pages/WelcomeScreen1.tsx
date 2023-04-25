24e7d04cbbfbfb4ce9f51aba6f8b304e2bceef05
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-04-25 22:09:03 +0530

    welcomeScreen1 bug fuxed

--- pages/WelcomeScreen1.tsx
+++ pages/WelcomeScreen1.tsx
@@ -1,12 +1,13 @@
-const name = "Andrew Bate";
-
-export const WelcomePage = () => {
+const WelcomePage = (props:any) => {
   return (
     <div className="bg-[#161B21] flex flex-col h-screen items-center justify-center">
      
       <p className="font-generalsans font-extralight text-base md:text-lg lg:text-5xl leading-normal text-center text-white">
-        Hello, {name}
+        Hello, {props.name}
       </p>
     </div>
   );
 };
+
+export default WelcomePage
+
