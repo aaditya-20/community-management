b4eee4b38fb0e610d4025f573dad3617aedd2b3f
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-04-26 22:04:19 +0530

    small fix in welcome screen

--- pages/WelcomeScreen1.tsx
+++ pages/WelcomeScreen1.tsx
@@ -17,7 +17,7 @@ const WelcomePage = () => {
     <div className="bg-[#161B21] flex flex-col h-screen items-center justify-center">
      
       <p className="font-generalsans font-extralight text-base md:text-lg lg:text-5xl leading-normal text-center text-white">
-        Hello, ${name}
+        Hello,{name}
       </p>
     </div>
   );
