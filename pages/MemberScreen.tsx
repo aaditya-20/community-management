bfa84baf9106aad00d4ae97ad3ba82c0cdf2f995
Aaditya <codingaadi@gmail.com>
2023-04-27 00:09:17 +0530

    feat: :sparkles: added MissionOnboardingMission page
    added a page and done some modification in member screen

--- pages/MemberScreen.tsx
+++ pages/MemberScreen.tsx
@@ -6,7 +6,7 @@ import { BsChevronDown } from "react-icons/bs";
 export default function MemberScreen() {
   return (
     <>
-      <div className='grid grid-cols-[auto_auto] gap-0  bg-[#171C23] grid-rows-[71px_auto] h-[950px] w-[1350px]'>
+      <div className='grid grid-cols-[auto,auto] gap-0  bg-[#171C23] grid-rows-[71px,auto] h-[auto] w-[auto]    md:grid-cols-[auto,1fr]'>
         {/* sidebar */}
         <div className='row-span-3 border-r-[1px] border-r-[#353B43]'>
             <Sidebar/>
@@ -19,10 +19,10 @@ export default function MemberScreen() {
         </div>
         {/* --header-- */}
 
-        <div className='ml-[10px]'>
-            <h1 className='text-[#FFFFFF] font-[600] text-[24px] my-[10px] mx-[20px]'>Community members</h1>
+        <div className='ml-[10px] h-[auto] w-[auto] bg-[#171C23]'>
+            <div className='text-[#FFFFFF] font-[600] text-[24px] my-[10px] mx-[20px]'>Community members</div>
 
-            <div className='flex mx-[20px] mb-[30px] '>
+            <div className='flex mx-[20px] mb-[30px] w-[auto] bg-[#171C23] '>
               <p className='text-[#A6A6A6] font-[400] text-[16px]'>check who is ahead of evryone</p>
               <button className='absolute right-[20px] flex items-center justify-evenly mb-[10px] border-[1px] border-[#757575]  text-[#757575] rounded-[8px] w-[109px] h-[33px]'>Last Week <BsChevronDown color='#757575' size={15} className=''/></button>
              
