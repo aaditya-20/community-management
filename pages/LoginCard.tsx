af5dffbe3aa8b523e29955066c72c29da7dac933
Aaditya <codingaadi@gmail.com>
2023-04-28 23:35:35 +0530

    fix: :bug: fix a bug of log in card

--- pages/LoginCard.tsx
+++ pages/LoginCard.tsx
@@ -26,7 +26,7 @@ const LoginCard = () => {
           <MagicLinkPopup />
         </div>
       </Modal>
-    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
+    <div className="absolute left-[40vw] top-[50vh] flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
       <div className="card w-[420px] bg-neutral text-neutral-content">
         <div className="card-body items-center text-center">
           <h2 className="card-title mb-3">Login Here!</h2>
