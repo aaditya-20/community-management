6c9c175508e5c0a1ed9fb92fbebd113a7b944fdf
Aps2612 <apsingh1722@gmail.com>
2023-05-01 00:39:27 +0530

    Fixed bug of storing in supabase

--- components/atoms/TextArea.tsx
+++ components/atoms/TextArea.tsx
@@ -1,7 +1,9 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement, useState } from "react";
 
 type TextAreaProps = {
+  handleChange2: (e: any) => void;
   label: string;
+  handleValue: string;
   placeholder?: string;
   className?: string;
   classNameLabel?: string;
@@ -10,13 +12,15 @@ type TextAreaProps = {
 
 const TextArea = ({
   label,
-  placeholder = '',
-  className = '',
-  classNameLabel = '',
-  classNameInput = '',
+  placeholder = "",
+  className = "",
+  classNameLabel = "",
+  classNameInput = "",
+  handleChange2,
+  handleValue = "",
 }: TextAreaProps): ReactElement => {
   // state to hold the current input value
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   // function to handle input change events
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -28,15 +32,17 @@ const TextArea = ({
   return (
     <div className={`flex flex-col ${className}`}>
       {/* label for the textarea */}
-      <label className={`text-sm font-medium text-gray-700 ${classNameLabel}`}>{label}</label>
+      <label className={`text-sm font-medium text-gray-700 ${classNameLabel}`}>
+        {label}
+      </label>
 
       {/* the textarea element itself */}
       <textarea
-        value={value} // set the value of the textarea to the current state
-        onChange={handleChange} // call handleChange on input change events
+        value={handleValue} // set the value of the input to the current state
+        onChange={handleChange2} // call handleChange on input change events
         placeholder={placeholder}
         className={`px-3 py-2 mt-1 overflow-hidden text-white font-small ${classNameInput}`}
-        style={{ height: '150px', resize: 'none' }} // set the height of the textarea
+        style={{ height: "150px", resize: "none" }} // set the height of the textarea
       />
     </div>
   );
