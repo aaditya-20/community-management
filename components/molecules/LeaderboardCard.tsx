1d9921687b8084e7f11cbf45a4c78a90b3f7f45f
Aman kumar jaiswal <iit11aman@gmail.com>
2023-05-03 14:46:30 +0530

    Leaderboard page and user dashboard : your space

--- components/molecules/LeaderboardCard.tsx
+++ components/molecules/LeaderboardCard.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image';
 const LeaderboardCard = (props:any) => {
   return (
     <div className="
-    bg-black absolute w-230 h-270 left-290 top-195">
+    bg-black absolute w-[230px] h-[270px]">
           <div className="flex m-2">
           <div><Image src="/leadericon.png" alt="hell" width={66} height={24} className="m-4 p-0 bg-black"/ ></div>
           <div className=" w-61 h-24 font-sans italic font-extrabold text-5xl leading-14 bg-gradient-to-b from-orange-500 to-orange-700 bg-clip-text text-transparent">{props.rank}</div>
           </div>
-          <div className="w-22 h-19 left-156 top-10 bg-white bg-clip-text text-transparent">{props.name}</div>
-          <div className="m-3 w-22 h-19 left-156 top-10 bg-white bg-clip-text text-transparent">{props.point}</div>
+          <div className="absolute w-[92px] h-[24px] left-[28px] top-[157px] bg-white bg-clip-text text-transparent">{props.name}</div>
+
+          <div className="abslute  w-[61px] h-[24px] left-[81px] top-[322px] bg-white bg-clip-text text-transparent">{props.point} XP</div>
         </div>
   )
 }
--- pages/LeaderBoardScreen.tsx
+++ pages/LeaderBoardScreen.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@/components/molecules/Sidebar";
 import Header from "@/components/atoms/Header";
 import LeaderboardCard from "@/components/molecules/LeaderboardCard";
 import TopContributorCardLeaderboardScreen from "@/components/molecules/TopContributorCardLeaderboardScreen";
-
+import { BsChevronDown } from "react-icons/bs";
 
 const cards = [
   {
@@ -45,19 +45,22 @@ const Leaderboard = () => {
             <h2 className="text-white text-2xl font-bold absolute left-[285px] top-[80px] mb-2">
               Leaderboard
             </h2>
-            <div className="text-gray-400 text-medium absolute left-[285px] top-[110px]">
+            <div className="flex m-[20px] text-gray-400 text-medium absolute left-[285px] top-[110px]">
               Check who is ahead of everyone
+            </div>
+            <div>
+            <button className='absolute right-[20px] flex items-center justify-evenly mb-[10px] border-[1px] border-[#757575]  text-[#757575] rounded-[8px] w-[109px] h-[33px]'>Last Week <BsChevronDown color='#757575' size={15} className=''/></button>
             </div>
           </div>
           <div className=" bg-gray-900 py-0 px-0 flex flex-col flex-0">
             <div className=" flex flex-1 ">
               <div className="bg-gray-900  rounded-lg p-6 flex flex-col space-y-4">
-              <div className="flex flex-row m-4 justify-between overflow py-[17px] px-[px]">
-      <div className="m-20 px-[10px] p-2"> <LeaderboardCard name="AMAN JAISWAL" point='1000' rank='1st'/></div>
-      <div className="m-20 px-[10px] p-2"> <LeaderboardCard name="AJAY" point='900' rank='2nd'/></div>
-      <div className="m-20 px-[10px] p-2 "> <LeaderboardCard name="NAYAK" point='500' rank='3rd'/></div>
-      <div className="m-20 px-[10px] p-2 "> <LeaderboardCard name="NAMAN" point='300' rank='4th'/></div>
-      <div className="m-20 px-[10px] p-2 "> <LeaderboardCard name="HARRY" point='100' rank='5th'/></div>
+              <div className="flex ">
+      <div className="absolute m-20 px-[10px] left-[190px] "> <LeaderboardCard name="AMAN" point='1000' rank='1st'/></div>
+      <div className="absolute m-20 px-[10px] left-[430px] "> <LeaderboardCard name="AJAY" point='900' rank='2nd'/></div>
+      <div className="absolute m-20 px-[10px] left-[670px] "> <LeaderboardCard name="NAYAK" point='500' rank='3rd'/></div>
+      <div className="absolute m-20 px-[10px] left-[910px] "> <LeaderboardCard name="NAMAN" point='300' rank='4th'/></div>
+      <div className="absolute m-20 px-[10px] left-[1150px]"> <LeaderboardCard name="HARRY" point='100' rank='5th'/></div>
     </div>
         </div>
                 <div className="absolute left-[-95px] top-[40px]">
@@ -69,9 +72,6 @@ const Leaderboard = () => {
             </div>
           </div>
         </div>
-        <h1 className="absolute left-[1210px] top-[80px] text-white text-xl font-bold">
-          Button with drop down
-        </h1>
       </div>
   );
 };
