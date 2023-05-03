9757761383a17e4c00edeee3374fdf2a3ad892b6
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2023-05-03 14:55:25 +0530

    Update LeaderboardCard.tsx

--- components/molecules/LeaderboardCard.tsx
+++ components/molecules/LeaderboardCard.tsx
@@ -9,7 +9,6 @@ const LeaderboardCard = (props:any) => {
           <div className=" w-61 h-24 font-sans italic font-extrabold text-5xl leading-14 bg-gradient-to-b from-orange-500 to-orange-700 bg-clip-text text-transparent">{props.rank}</div>
           </div>
           <div className="absolute w-[92px] h-[24px] left-[28px] top-[157px] bg-white bg-clip-text text-transparent">{props.name}</div>
-
           <div className="abslute  w-[61px] h-[24px] left-[81px] top-[322px] bg-white bg-clip-text text-transparent">{props.point} XP</div>
         </div>
   )
