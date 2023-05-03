e8d89eddba83e0788d9ce01a2b44724d8cde6442
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-05-03 17:09:32 +0530

    rounting added in mission template

--- components/molecules/SmallMissionCard.tsx
+++ components/molecules/SmallMissionCard.tsx
@@ -1,15 +1,19 @@
+import { Router, useRouter } from "next/router";
 import React from "react";
 interface SmallMissionCardProps {
   missionTitle: String;
   missionDiscription: String;
   usdc: String;
+  page: any
 }
 
 const SmallMissionCard: React.FC<SmallMissionCardProps> = ({
   missionDiscription,
   missionTitle,
   usdc,
+  page,
 }) => {
+  const router = useRouter();
   return (
     <div className="w-[364px] h-[95px] rounded-[10px] p-[19px] border-[1px] bg-[#232B35] border-[rgb(117,117,117)]/[0.04] flex justify-between items-center">
       <div className="h-full w-full flex justify-between">
@@ -19,7 +23,9 @@ const SmallMissionCard: React.FC<SmallMissionCardProps> = ({
             Mision: {missionDiscription}
           </h2>
         </div>
-        <button className="w-[70px] h-[30px] bg-[rgb(255,186,3)]/[0.36] rounded-[8px] flex justify-center items-center">
+        <button onClick={() => {
+          router.push(page)
+        }} className="w-[70px] h-[30px] bg-[rgb(255,186,3)]/[0.36] rounded-[8px] flex justify-center items-center">
           <h1 className="font-bold text-[12px] leading-[14px] text-[#FFBA03]">
             {usdc} USDC
           </h1>
