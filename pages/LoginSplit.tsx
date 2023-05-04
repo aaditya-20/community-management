84b4a95409ad377a49ed1d228bc4afe1fb1fa6dd
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-05-04 06:02:21 +0530

    Added Login Popups in First page

--- pages/LoginSplit.tsx
+++ pages/LoginSplit.tsx
@@ -1,8 +1,27 @@
 import router from "next/router";
 import React, { useState } from "react";
+import LoginSection from "./LoginSection";
+import Modal from "@material-ui/core/Modal";
+
 const LoginSplit = () => {
+  const [OpenAdmin, setOpenAdmin] = useState(false);
+  function handleAdminClick() {
+    setOpenAdmin(!OpenAdmin);
+  }
+
   return (
     <>
+      <Modal
+        onClose={() => {
+          setOpenAdmin(!OpenAdmin);
+        }}
+        open={OpenAdmin}
+        style={{}}
+      >
+        <div>
+          <LoginSection />
+        </div>
+      </Modal>
       <div className="flex justify-center items-center h-screen">
         <div className="bg-[#232B35] h-auto w-[485px] shadow-[6px,6px,20px,rgba(15,15,15,0.26)]">
           <div className="w-full h-[54px] border-b border-[#353B43] flex items-center pl-[30px]">
@@ -11,7 +30,10 @@ const LoginSplit = () => {
             </h1>
           </div>
           <div className="w-full h-auto pr-[50px] pl-[30px] pb-[29px] pt-[37px]">
-            <button className="w-full h-[45px] bg-white border border-[#EAEAEA] flex justify-center items-center rounded-[9px] mb-6">
+            <button
+              onClick={handleAdminClick}
+              className="w-full h-[45px] bg-white border border-[#EAEAEA] flex justify-center items-center rounded-[9px] mb-6"
+            >
               <div className="flex gap-[14px] items-center justify-center">
                 <h1 className="font-medium text-xs text-black">
                   Community Admin
