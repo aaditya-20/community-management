06d43074783aabd863c91a22e8c630066bc5616e
Aps2612 <apsingh1722@gmail.com>
2023-05-07 15:50:07 +0530

    Added some Template pages

--- pages/MissionDigitalMascot.tsx
+++ pages/MissionDigitalMascot.tsx
@@ -1,135 +1,56 @@
-import React from "react";
-import Sidebar from "@/components/molecules/Sidebar";
-import Header from "@/components/atoms/Header";
-import NavbarMissionOnboarding from "@/components/molecules/NavbarMissionOnboarding";
-import BeAchamp from "@/components/molecules/BeAchamp";
-import MissionStepsCard from "@/components/molecules/MissionStepCard";
-import Image from "next/image";
-import { useState } from "react";
-import { supabase } from "@/utils/supabaseClient";
-
+import React from 'react'
+import Sidebar from '@/components/molecules/Sidebar';
+import Header from '@/components/atoms/Header';
+import NavbarMissionOnboarding from '@/components/molecules/NavbarMissionOnboarding';
+import BeAchamp from '@/components/molecules/BeAchamp';
+import MissionStepsCard from '@/components/molecules/MissionStepCard';
+import Image from 'next/image';
 export default function MissionDigitalMascot() {
-  const[project,setProject] = useState('')
-  const[file,setFile] = useState(null)
-  const bucket_name = 'Store';
-
-  async function handleSubmit(){
-    const input = document.createElement("input");
-    input.type = "file";
-    input.accept = ".png, .jpg, .jpeg";
-    input.hidden = true;
-    input.addEventListener("change", async () => {
-      const files = input.files;
-      if (files && files.length > 0) {
-        const file = files[0];
-        console.log(file);
-        const { data, error } = await supabase.storage
-          .from(bucket_name)
-          .upload(`digital_mascot/${file.name}`, file, {
-            cacheControl: "3600",
-            upsert: false,
-          });
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(data);
-        }
-      }
-    });
-    input.click();
-  }
-
- function handleProject(e:any){
-    setProject(e.target.value)
- }
-
- function handleChange(e : any) {
-    setFile(e.target.files[0]);
-  }
-
- 
-
- 
- 
-  
-  
-
   return (
     <div>
-      <div className="grid grid-cols-[auto,auto] gap-0  bg-[#171C23] grid-rows-[71px,100px,auto] h-[1500px] w-[1400px] md:grid-cols-[auto,1fr]">
+        <div className="grid grid-cols-[auto,auto] gap-0  bg-[#171C23] grid-rows-[71px,100px,auto] h-[1500px] w-[1400px] md:grid-cols-[auto,1fr]">
         <div className="row-span-3 border-r-[1px] border-r-[#353B43]">
-          <Sidebar />
+            <Sidebar />
         </div>
         <div className="border-b-[1px] border-b-[#353B43]">
-          <Header />
+            <Header />
         </div>
-        <div className="">
+        <div className=''>
           <NavbarMissionOnboarding />
         </div>
-        <div className="flex">
-          <div className="text-[#ffffff] ml-[20px] mb-[10px] w-[auto]  border-[1px] border-[#353B43] rounded-[20px]">
-            <Image
-              src="/../public/Icons/MissionPageBanner.png"
-              width={800}
-              height={1}
-              alt="kjdfhah"
-              className="mx-[10px] my-[10px]"
-            />
-            <div className="mx-[30px] my-[30px]">
-              <div className="font-[600] text-[24px] text-[#ffffff] mb-3">
-                Design our company digital mascot
-              </div>
-              <form onSubmit={handleSubmit} className="w-full max-w-lg">
-                <div className="mb-5">
-                  <label
-                    htmlFor="icon"
-                    className="block text-white-700 font-bold "
-                  >
-                    Name Of Project
-                  </label>
-                  <input
-                    type="text"
-                    id="icon"
-                    name="icon"
-                    value={project}
-                    onChange={handleProject}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="file-upload">
-                    Upload a GIF or WEBP file:
-                  </label>
-                  <input id="file-upload" type="file" accept=".gif,.webp" onChange={handleChange} />
-                  <button
-                    onClick={handleSubmit}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    style={{
-                      top: "0px",
-                      left: "0px",
-                      position: "relative",
-                    }}
-                    type="submit"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+        <div className='flex'>
+            <div className='text-[#ffffff] ml-[20px] mb-[10px] w-[auto]  border-[1px] border-[#353B43] rounded-[20px]'>
 
-              <Image
-                src="/../public/Icons/DigitalMascot.png"
-                width={255}
-                height={255}
-                alt="kjdfhah"
-                className="mx-[auto] my-[30px] mb-[10px]"
-              />
-            </div>
-          </div>
-          <div className="text-[#ffffff] w-[auto] h-[auto] ml-[20px]">
-            <BeAchamp val="1000" />
-          </div>
+                <Image src="/../public/Icons/MissionPageBanner.png" width={800} height={1} alt='kjdfhah' className='mx-[10px] my-[10px]'/>
+                <div className='mx-[30px] my-[30px]'>
+                    <div className='font-[600] text-[24px] text-[#ffffff]'>Design our company digital mascot</div>
+                    <div className='font-[500] text-[16px] text-[#D9D9D9]'>We want to Design our company digital mascot
+                        <ol type="1" className='font-[General Sans] list-decimal mx-[20px]'>
+                            <li>Requirements:</li>
+                            <li>the body of the NFT design consists of a combination of our icon and as gemstones, gold, etc.</li>
+                            <li>The design should include the project name: Mew Protocol</li>
+                            <li> animation effects. Format can be GIF, WEBP, etc.</li>
+                            <li>the NFT design should be consistent with the design style of our project</li>
+                            <li>We will choose 3 prizes: $40 for the first place, $10 for the second and the third place.</li>
+                            <li>Reference:</li>
+                            <li>www.mewprotocol.com</li>
+                        </ol>            
+                    </div>  
+
+                <Image src="/../public/Icons/DigitalMascot.png" width={255} height={255} alt='kjdfhah' className='mx-[auto] my-[30px] mb-[10px]'/>
+
+                </div>
+                <div className='mx-[30px] my-[30px]'>
+                <MissionStepsCard className="mx-[20px]" heading1="Onboard 10 new members.................................................................                      " heading2="Submit mission" descp1="Invite new members..........................................................." descp2="Share the proof of work" />  
+                </div>
+
+                </div>
+            <div className='text-[#ffffff] w-[auto] h-[auto] ml-[20px]'><BeAchamp val="1000"/></div>
         </div>
-      </div>
+
+
+
     </div>
-  );
+    </div>
+  )
 }
--- pages/MissionShareYourFeedback.tsx
+++ pages/MissionShareYourFeedback.tsx
@@ -5,44 +5,14 @@ import NavbarMissionOnboarding from "@/components/molecules/NavbarMissionOnboard
 import BeAchamp from "@/components/molecules/BeAchamp";
 import MissionStepsCard from "@/components/molecules/MissionStepCard";
 import Image from "next/image";
-import TextInput from "@/components/atoms/TextInput";
-import { useState } from "react";
-import { supabase } from "@/utils/supabaseClient";
-import TextArea from "@/components/atoms/TextArea";
-import ShareFeedbackCard from "@/components/molecules/ShareFeedbackCard";
 export default function MissionShareYourFeedback() {
-  const [input, setInput] = useState("");
-
-  function handleInput(e: any) {
-    setInput(e.target.value);
-  }
-
-  const bucket_name = "Store";
-  async function handleUpload() {
-    const input = document.createElement("input");
-    input.type = "file";
-    input.accept = ".png, .jpg, .jpeg";
-    input.hidden = true;
-    input.addEventListener("change", async () => {
-      const files = input.files;
-      if (files && files.length > 0) {
-        const file = files[0];
-        console.log(file);
-        const { data, error } = await supabase.storage
-          .from(bucket_name)
-          .upload(`feedback_share/${file.name}`, file, {
-            cacheControl: "3600",
-            upsert: false,
-          });
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(data);
-        }
-      }
-    });
-    input.click();
-  }
+  const title = "Share Your Feedback";
+  const description =
+    "Share your feedback about our product on Product Hunt and upload a screenshot.";
+  const tags = ["onboarding"];
+  const missionSteps = ["Onboard 10 new Members", "Submit Mission"];
+  const reward = 1000;
+  const coinType = 'USDC'
   return (
     <div>
       <div className="grid grid-cols-[auto,auto] gap-0  bg-[#171C23] grid-rows-[71px,100px,auto] h-[1500px] w-full md:grid-cols-[auto,1fr]">
@@ -64,36 +34,27 @@ export default function MissionShareYourFeedback() {
               alt="kjdfhah"
               className="mx-[10px] my-[10px]"
             />
-            <div className="mx-[-60px] my-[50px] left-[40vh]">
-              <TextInput
-                placeholder="Write your Feedback Here"
-                label="Share Your Feedback"
-                className="relative top-[-40px] left-[100px] w-[500px] h-[41px]"
-                classNameInput="w-[500px] h-[41px] bg-[#2E363F] text-white"
-                classNameLabel="font-medium text-white leading-6 text-white font-generalsans w-[250px] h-[22px] mb-2"
-                handleChange2={handleInput}
-                handleValue={input}
-              />
-
-              <div className="mx-[100px] my-[10px]">
-                <h2 className="mb-[10px]">Upload a Screenshot</h2>
-                <div className="flex items-center">
-                  {/* <input type="file" accept="image/*" className="mb-[10px]" /> */}
-                  <button
-                    onClick={handleUpload}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Upload
-                  </button>
-                </div>
+            <div className="mx-[30px] my-[30px]">
+              <div className="font-[600] text-[24px] text-[#ffffff]">
+                Share your feedback
               </div>
+              <div className="font-[500] text-[16px] text-[#D9D9D9]">
+                Share your feedback about our product on Product Hunt and upload
+                a screenshot.
+              </div>
+            </div>
+            <div className="mx-[30px] my-[30px]">
+              <MissionStepsCard
+                className="mx-[20px]"
+                heading1="Onboard 10 new members.................................................................                      "
+                heading2="Submit mission"
+                descp1="Invite new members..........................................................."
+                descp2="Share the proof of work"
+              />
             </div>
           </div>
           <div className="text-[#ffffff] w-[auto] h-[auto] ml-[20px]">
-            <ShareFeedbackCard
-              feedback="Share your feedback for Firebond"
-              tag = {['Writing','Marketing']}
-              val="2.9"
-            />
+            <BeAchamp val="1000" />
           </div>
         </div>
       </div>
