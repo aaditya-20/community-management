32e8e11e6c0f16a969ee95123c05136c9a2765a8
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-05-09 05:32:04 +0530

    added BeAChamp card

--- components/molecules/BeAchamp.tsx
+++ components/molecules/BeAchamp.tsx
@@ -1,28 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const BeAchamp = (props:any) => {
+const BeAchamp = (props: any) => {
+
   return (
-    <div className="bg-gray-800 absolute w-56 h-270 left-290 top-195 rounded-5 ">
-        <h1 className="text-xl font-bold mb-4 text-white">Be A Champion</h1>
-        <div className="bg-gray-600"><hr ></hr></div>
-        <div className='flex'>
-            <div>
-                <div  className="p-1 m-2 bg-gray font- mb-3 text-white ">
-                    <p className='text-s text-gray-400 h-10'> Reward</p>
-                </div>
-            <div>
-            <div  className="m-3 bg-gray font- mb-4 text-white ">
-                    <p className='text-s text-gray-400 h-10'>Tags </p>
-                 </div>
+    <div className="bg-gray-800 absolute w-[346px] h-[267px] left-[1142px] top-[219px] rounded-2xl ">
+      <div className="text-2xl font-bold mb-4 text-white w-[292px] h-[64px] left-[1166px] top-[242px] ml-[15px] mt-[15px]">
+        {props.title}
+      </div>
+      <div className="text-gray-500">
+        <hr className="border-gray-500" />
+      </div>
+      <div className="flex ">
+        <div>
+          <div>
+            <div className="inline-flex items-center px-2 py-1 bg-gray mb-3 text-white mt-2 mr-2">
+              <span className="inline-flex items-center px-2 py-1 bg-gray mb-3 text-white mt-2">
+                <span className="text-sm text-gray-400 mr-2">Reward</span>
+                <span className="ml-2">
+                  <button className="inline-flex items-center justify-center bg-yellow-600 rounded-lg w-20 h-8 text-yellow-400 font-bold">
+                    <p className="text-sm">{props.val} USDC</p>
+                  </button>
+                </span>
+              </span>
             </div>
-            </div>
-            <div>
-        <button className="bg-yellow-600  border- rounded-[8px] w-[103px] h-[34px] text-yellow m-1 p-1 font-bold mb-3 text-yellow-400 h-8 w-20"><p> {props.val} USDC </p></button>
-        <button className="bg-gray-600 text-m m-1 p-1 font- mb-3 text-white h-8 w-30"><p>📢onboarding </p></button>
+          </div>
+          <div className="inline-flex">
+            <span className="m-3 bg-gray mb-4 text-white mr-2">
+              <p className="text-sm text-gray-400 mr-2">Tags</p>
+            </span>
+            {props.tags.map((names: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined) => (
+              <span>
+                <button className="bg-gray-600 text-m m-1 p-1 mb-3 text-white h-8 w-30">
+                  <p>{names}</p>
+                </button>
+              </span>
+            ))}
+          </div>
         </div>
-        </div>
-        </div>
-  )
-}
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default BeAchamp
+export default BeAchamp;
