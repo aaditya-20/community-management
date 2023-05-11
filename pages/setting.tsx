111ca070ce5cbdcd1df54fb4fce172085f7f79c2
crysosancher <vibhupandey.vp@gmail.com>
2023-05-12 01:54:12 +0530

    Error resolved

--- pages/setting.tsx
+++ pages/setting.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // console.log(useState)
 import Sidebar from "../components/molecules/Sidebar";
 import Header from "../components/atoms/Header";
@@ -9,96 +9,99 @@ import { supabase } from "../utils/supabaseClient";
 
 
 export default function Home() {
-  //supabase code retriving users informtion from database
-  const storedJsonData = localStorage.getItem('data');
-const jsonData = JSON.parse(storedJsonData ?? '{}');
-let name = jsonData.name;
-let emailId = jsonData.email;
-let walletId = jsonData.wallet_id;
-// console.log(name,emailId,walletId)
+  const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [wallet, setWallet] = useState("");
 
-  const [username, setUsername] = useState(name);
-  const [email, setEmail] = useState(emailId);
-  const [wallet, setWallet] = useState(walletId);
-const handleInputChange = (event:any) => {
-  setUsername(event.target.value);
-}
-const handelEmailChange = (event:any) => {
-  setEmail(event.target.value);
-}
-const handleWalletChange = (event:any) => {
-  setWallet(event.target.value);
-}
-const handelEmailClick = async() => {
-  const { data:dataUpdate, error:updateError } = await supabase
-  .from('community_data')
-  .update({ email: `${emailId}` })
-  .eq('wallet_id', walletId)
-  if(updateError){
-    alert("Error")
-  }else{
-    alert(`Username Updated Successfully ${email}`)
-  }
-  // localStorage.setItem("email",email);
-  // alert("Email updated successfully");
-  
-}
-//Updating username
-const handleUpdateClick=async ()=>{
-  const { data:dataUpdate, error:updateError } = await supabase
-  .from('community_data')
-  .update({ name: `${username}` })
-  .eq('wallet_id', walletId)
-  if(updateError){
-    alert("Error")
-  }else{
-    alert(`Username Updated Successfully:${username}`)
-  }
-  //localStorage.setItem("username",username);
-  // alert("Username updated successfully");
-}
+  if (typeof window !== 'undefined') {
+    //supabase code retriving users informtion from database
+    const storedJsonData = localStorage.getItem('data');
+    const jsonData = JSON.parse(storedJsonData ?? '{}');
+    let name = jsonData.name;
+    let emailId = jsonData.email;
+    let walletId = jsonData.wallet_id;
+    // console.log(name,emailId,walletId)
 
-const handelWalletClick = async() => {
-  const { data:dataUpdate, error:updateError } = await supabase
-  .from('community_data')
-  .update({ wallet_id: `${walletId}` })
-  .eq('wallet_id', walletId)
-  if(updateError){
-    alert("Error")
-  }else{
-    alert(`Wallet Address Updated Successfully with:${wallet}`)
-  }
-  // localStorage.setItem("wallet",wallet);
-  // alert("Wallet updated successfully");
-}
-const handelDelete=async()=>{
-  const { data:dataUpdate, error:updateError } = await supabase
-  .from('community_data')
-  .delete()
-  .eq('wallet_id', walletId)
+    
+    const handleInputChange = (event: any) => {
+      setUsername(event.target.value);
+    }
+    const handelEmailChange = (event: any) => {
+      setEmail(event.target.value);
+    }
+    const handleWalletChange = (event: any) => {
+      setWallet(event.target.value);
+    }
+    const handelEmailClick = async () => {
+      const { data: dataUpdate, error: updateError } = await supabase
+        .from('community_data')
+        .update({ email: `${emailId}` })
+        .eq('wallet_id', walletId)
+      if (updateError) {
+        alert("Error")
+      } else {
+        alert(`Username Updated Successfully ${email}`)
+      }
+      // localStorage.setItem("email",email);
+      // alert("Email updated successfully");
 
-if(confirm("Are you sure you want to delete your account?")){
-  if(updateError){
-    alert("Error")
-  }else{
-    alert(`Account Deleted Successfully`)
-  }
-  localStorage.clear();
-  window.location.href = "/";
-}
-}
+    }
+    //Updating username
+    const handleUpdateClick = async () => {
+      const { data: dataUpdate, error: updateError } = await supabase
+        .from('community_data')
+        .update({ name: `${username}` })
+        .eq('wallet_id', walletId)
+      if (updateError) {
+        alert("Error")
+      } else {
+        alert(`Username Updated Successfully:${username}`)
+      }
+      //localStorage.setItem("username",username);
+      // alert("Username updated successfully");
+    }
 
-  return (
-    <>
-      {/* <header>
+    const handelWalletClick = async () => {
+      const { data: dataUpdate, error: updateError } = await supabase
+        .from('community_data')
+        .update({ wallet_id: `${walletId}` })
+        .eq('wallet_id', walletId)
+      if (updateError) {
+        alert("Error")
+      } else {
+        alert(`Wallet Address Updated Successfully with:${wallet}`)
+      }
+      // localStorage.setItem("wallet",wallet);
+      // alert("Wallet updated successfully");
+    }
+    const handelDelete = async () => {
+      const { data: dataUpdate, error: updateError } = await supabase
+        .from('community_data')
+        .delete()
+        .eq('wallet_id', walletId)
+
+      if (confirm("Are you sure you want to delete your account?")) {
+        if (updateError) {
+          alert("Error")
+        } else {
+          alert(`Account Deleted Successfully`)
+        }
+        localStorage.clear();
+        window.location.href = "/";
+      }
+    }
+
+    return (
+      <>
+        {/* <header>
         <span className="heading">User Profile</span>
         <button> Logout </button>
       </header> */}
-      <Header />
+        <Header />
 
-      <div className="main-div">
-        <nav>
-          {/* <div>
+        <div className="main-div">
+          <nav>
+            {/* <div>
             {" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -141,65 +144,68 @@ if(confirm("Are you sure you want to delete your account?")){
             </svg>
             Leaderboard
           </div> */}
-          <Sidebar />
-        </nav>
+            <Sidebar />
+          </nav>
 
-        <main>
-          <div>
-            <section className="first-section">
-              <div className="profile-img">
-                <img src="https://picsum.photos/200/300" alt="profile" />
-              </div>
-              <div className="links-zone">
-                <span> mial.com </span>
-                <span> discord </span>
-                <button> Twitter </button>
-                <button> LinkedIn </button>
-              </div>
-            </section>
+          <main>
+            <div>
+              <section className="first-section">
+                <div className="profile-img">
+                  <img src="https://picsum.photos/200/300" alt="profile" />
+                </div>
+                <div className="links-zone">
+                  <span> mial.com </span>
+                  <span> discord </span>
+                  <button> Twitter </button>
+                  <button> LinkedIn </button>
+                </div>
+              </section>
 
-            <label className="label">
-              Username <br />
-              <input type="text" value={username} onChange={handleInputChange} />
-              <button className="updateButton" onClick={handleUpdateClick}> Update </button>
-            
-            </label>
+              <label className="label">
+                Username <br />
+                <input type="text" value={username} onChange={handleInputChange} placeholder="Jhon" />
+                <button className="updateButton" onClick={handleUpdateClick}> Update </button>
 
-            <label className="label">
-              Email <address></address> 
-              <input type="email" value={email} onChange={handelEmailChange}/>
-              <button className="updateButton" onClick={handelEmailClick}> Update </button>
-            </label>
-            <span>
-              {" "}
-              Please enter your address so that we can send you web3 newslater{" "}
-            </span>
+              </label>
 
-            <section className="second-section">
-              <div> Reward Section </div>
-              {/* <span>
+              <label className="label">
+                Email <address></address>
+                <input type="email" value={email} onChange={handelEmailChange} placeholder="Jhon@wick.com" />
+                <button className="updateButton" onClick={handelEmailClick}> Update </button>
+              </label>
+              <span>
+                {" "}
+                Please enter your address so that we can send you web3 newslater{" "}
+              </span>
+
+              <section className="second-section">
+                <div> Reward Section </div>
+                {/* <span>
                 {" "}
                 Choose which information to display on yo9ur zelay profile{" "}
               </span> */}
-              <label className="label">
-              Wallet Address <address></address> 
-              <input type="text" value={wallet} onChange={handleWalletChange}/>
+                <label className="label">
+                  Wallet Address <address></address>
+                  <input type="text" value={wallet} onChange={handleWalletChange} placeholder="0*johnaddress" />
 
-              <button className="updateButton" onClick={handelWalletClick}> Update </button>
-            </label>
-            <span>
-              {" "}
-              Please enter your address so that we can send you rewards{" "}
-            </span>
-            </section>
+                  <button className="updateButton" onClick={handelWalletClick}> Update </button>
+                </label>
+                <span>
+                  {" "}
+                  Please enter your address so that we can send you rewards{" "}
+                </span>
+              </section>
 
-            <section className="section-three">
-              <div> Danger Zone </div>
-              <button onClick={handelDelete}> Delete My Account </button>
-            </section>
-          </div>
-        </main>
-      </div>
-    </>
-  );
+              <section className="section-three">
+                <div> Danger Zone </div>
+                <button onClick={handelDelete}> Delete My Account </button>
+              </section>
+            </div>
+          </main>
+        </div>
+      </>
+    );
+
+  }
+
 }
