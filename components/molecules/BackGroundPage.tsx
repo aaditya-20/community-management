38948aa534bed8c20fdfb7c5c79eedb462cf8261
iam-sphinx <kandarp1803@gmail.com>
2023-05-22 20:27:14 +0530

    fix: :bug: fixed step1 community setup screen

--- components/molecules/BackGroundPage.tsx
+++ components/molecules/BackGroundPage.tsx
@@ -6,8 +6,8 @@ export default function BackGroundPage(props:any) {
    
         <>
    
-            <div className={`flex items-center justify-center bg-[#171C23] h-[1100px] w-[100vw]${props.className}`} >
-               <Image src="/Icons/FireBondIcon.png" alt="My Image" width={500} height={500} className=" absolute top-10 w-[193px] h-[68.1px] m-auto" />
+            <div className={`flex items-center justify-center bg-[#171C23] min-h-screen ${props.className}`} >
+               <Image src="/Icons/FIREBOND.svg" alt="My Image" width={500} height={500} className=" absolute top-10 w-[193px] h-[68.1px] m-auto" />
             </div>
           </>
   );
--- pages/Step1CommunitySetup.tsx
+++ pages/Step1CommunitySetup.tsx
@@ -1,4 +1,4 @@
-import { FaDiscord, FaTwitter } from "react-icons/fa";
+import { FaDiscord, FaGlobe, FaTwitter } from "react-icons/fa";
 import { VscBlank } from "react-icons/vsc";
 import IconButton from "../components/atoms/IconButton";
 import ProfileIcon from "../components/atoms/ProfileAvatar";
@@ -15,6 +15,9 @@ import { useRouter } from "next/router";
 import FormData from "@/utils/FormData";
 import Modal from "@material-ui/core/Modal";
 import TwitterHandlePopUp from "@/components/molecules/TwitterHandlePopUp";
+import { BiImageAdd } from "react-icons/bi";
+import { BsTwitter } from "react-icons/bs";
+import Image from "next/image";
 const Step1CommunitySetup = (): ReactElement => {
   const obj = FormData();
   const router = useRouter();
@@ -56,7 +59,7 @@ const Step1CommunitySetup = (): ReactElement => {
   async function CommunityLogoUpload(file: any) {
     const { data, error } = await supabase.storage
       .from("community_logo")
-      .upload(`${obj.email}` , file, {
+      .upload(`${obj.email}`, file, {
         cacheControl: "3600",
         upsert: false,
       });
@@ -75,7 +78,7 @@ const Step1CommunitySetup = (): ReactElement => {
   }
 
   function onContinueClick() {
-    CommunityLogoUpload(file)
+    CommunityLogoUpload(file);
     obj.community_name = InputValue;
     obj.community_description = description;
     obj.community_logo = community_logo;
@@ -139,8 +142,143 @@ const Step1CommunitySetup = (): ReactElement => {
           />
         </div>
       </Modal>
-      <BackGroundPage />
-      <div className="absolute  items-center m-[auto] top-[150px] justify-center right-0 left-0 w-[662px] h-[680px] bg-gray-800 shadow-md ">
+      <div className="min-h-screen bg-[#151a21]">
+        <div className="w-full flex justify-center items-center py-[19px]">
+          <Image src="Icons/FIREBOND.svg" height={68.1} width={193} alt="" />
+        </div>
+
+        <div className="flex justify-center items-center">
+          <div className="h-auto w-[662px] rounded-sm bg-[#232B35]">
+            <div className="h-[54px] w-full flex items-center justify-between px-[30px] border-b border-[#353B43]">
+              <h1 className="text-[#AEABD8] font-normal text-sm tracking-[16%]">
+                COMMUNITY SETUP
+              </h1>
+              <div className="flex gap-[31px] items-center">
+                <h1 className="text-[#FE702A] font-medium text-[13.1px] leading-[15.35px]">
+                  STEP 1 OF 2
+                </h1>
+
+                <div className="w-[108px] h-[12px] bg-[#FE702A54] border-[1.5px] border-[#FE702A]">
+                  <div className="w-[57px] h-full bg-[#FE702A]"></div>
+                </div>
+              </div>
+            </div>
+            {/* Main Content */}
+            <div className="w-full h-full px-[30px] pt-6 pb-[33px]">
+              <div className="w-full h-full">
+                <h1 className="font-semibold text-2xl text-white mb-2">
+                  Basic Details
+                </h1>
+                <h1 className="text-[#A6A6A6] font-normal text-base mb-[37px]">
+                  Help your members know more about your community
+                </h1>
+              </div>
+
+              {/* Name of your community */}
+              <div className="w-full h-auto flex gap-[14.5px] items-center mb-[43px]">
+                <div className="w-[65.5px] flex-shrink-0 h-[65.5px] cursor-pointer rounded-full border-[0.82px] border-[white] border-dashed flex justify-center items-center">
+                  <BiImageAdd size={32} />
+                </div>
+
+                <div className="w-full relative bottom-4">
+                  <TextInput
+                    placeholder="E.g. Fire Community"
+                    label="Name of your Community*"
+                    className=" w-full h-[41px]"
+                    classNameInput="w-full h-[41px] bg-[#2E363F] rounded-lg text-white"
+                    classNameLabel="font-medium text-base text-white mb-[10px]"
+                    handleChange2={handleInput}
+                    handleValue={InputValue}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full ">
+                <TextArea
+                  placeholder="Add description to let the members know more about your community"
+                  label="Whats the goal of the Community?"
+                  className=" w-full h-[250px]"
+                  classNameInput="w-full h-[250px] bg-[#2E363F] rounded-lg overflow-hidden font-small"
+                  classNameLabel="font-medium text-base text-white mb-[10px]"
+                  handleChange2={handleInput2}
+                  handleValue={description}
+                />
+              </div>
+
+              {/* Type of Community Section */}
+              <h1 className="font-medium text-base text-white mb-[9.28px]">
+                Choose type of community *
+              </h1>
+
+              <div className="flex gap-[13.1px] w-full mb-[41.77px]">
+                <button
+                  className="h-[40.94px] w-[65.5px] flex justify-center items-center text-white font-normal text-[13.1px] leading-[17.68px]  bg-[#2E363F] rounded-lg"
+                  onClick={() => handleButtonClick(1)}
+                >
+                  DAO
+                </button>
+                <button
+                  className="h-[40.94px] w-[122.81px] flex justify-center items-center text-white font-normal text-[13.1px] leading-[17.68px] bg-[#2E363F] rounded-lg"
+                  onClick={() => handleButtonClick(2)}
+                >
+                  NFT Community
+                </button>
+                <button
+                  className="h-[40.94px] w-[112.98px] flex justify-center items-center text-white font-normal text-[13.1px] leading-[17.68px] bg-[#2E363F] rounded-lg"
+                  onClick={() => handleButtonClick(3)}
+                >
+                  Content Creator
+                </button>
+                <button
+                  className="h-[40.94px] w-[65.5px] flex justify-center items-center text-white font-normal text-[13.1px] leading-[17.68px] bg-[#2E363F] rounded-lg"
+                  onClick={() => handleButtonClick(4)}
+                >
+                  Social
+                </button>
+              </div>
+              {/* Add Links */}
+
+              <h1 className="font-medium text-white text-base mb-[6.84px]">
+                Add Links
+              </h1>
+
+              <div className="flex gap-[13.1px] items-center">
+                <div
+                  className="w-[40.94px] h-[40.94px] rounded-full bg-[#1D1D1D] flex justify-center items-center cursor-pointer"
+                  onClick={handleGlobeClick}
+                >
+                  <FaGlobe className="text-white" size={28} />
+                </div>
+
+                <div
+                  className="w-[40.94px] h-[40.94px] rounded-full bg-[#1D1D1D] flex justify-center items-center cursor-pointer"
+                  onClick={handleTwitterClick}
+                >
+                  <BsTwitter className="text-white" size={24} />
+                </div>
+              </div>
+            </div>
+
+            {/* Back and Continue Section */}
+            <div className="h-[66px] w-full flex justify-between">
+              <button
+                className="w-full h-full flex justify-center items-center bg-[#1D1D1D] text-white font-normal text-base"
+                onClick={() => router.push("/CommunitySetupScreen")}
+              >
+                Back
+              </button>
+              <button
+                className="w-full h-full bg-[#FE702A] text-white font-base font-normal flex justify-center items-center"
+                onClick={onContinueClick}
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="absolute  items-center m-[auto] top-[150px] justify-center right-0 left-0 w-[662px] h-[680px] bg-gray-800 shadow-md ">
         <div className="relative w-[662px] h-[40px] border-b-[1px] border-[#353B43]">
           <Link
             href="/"
@@ -240,42 +378,42 @@ const Step1CommunitySetup = (): ReactElement => {
           </p>
 
           {/* <a href="https://www.example.com"> */}
-          <ProfileIcon
+      {/* <ProfileIcon
             size={30}
             imageUrl="/Globe.png"
             alt="nothing"
             classNameCircle="relative top-[-45px] left-[60px] border-[0.7px] mr-3"
             // classNameImage="relative left-[22px] top-[26px] w-[51.6px] h-[42.24px]"
             onProfileIconClick={handleGlobeClick}
-          />
-          {/* </a> */}
+          /> */}
+      {/* </a> */}
 
-          {/* <a href="https://www.example.com"> */}
-          <ProfileIcon
+      {/* <a href="https://www.example.com"> */}
+      {/* <ProfileIcon
             size={30}
             imageUrl="/Twitter.png"
             alt="nothing"
             classNameCircle="relative top-[-45px] left-[60px] border-[0.7px] mr-4"
             onProfileIconClick={handleTwitterClick}
-            // classNameImage="relative left-[22px] top-[26px] w-[51.6px] h-[42.24px]"
-          />
-          {/* </a> */}
-          <IconButton
+            // classNameImage="relative left-[22px] top-[26px] w-[51.6px] h-[42.24px]" */}
+
+      {/* </a> */}
+      {/* <IconButton
             icon={VscBlank}
             label="Back"
             className="relative bg-[#1D1D1D] top-[-25px] left-[0px] w-[340px] h-[60px]"
             classNameIcon=""
             onClick={() => router.push("/CommunitySetupScreen")}
-          />
-          <IconButton
+          /> */}
+      {/* <IconButton
             icon={VscBlank}
             label="Continue"
             className="relative bg-[#FE702A] top-[-85px] left-[335px] w-[338px] h-[60px]"
             classNameIcon=""
             onClick={onContinueClick}
           />
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </>
   );
 };
